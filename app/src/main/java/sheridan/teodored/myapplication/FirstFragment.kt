16350e7dd1dd7d1b21e9d58b8392a71fe302cc80
Steven Winstanley <winstanley.steven05@gmail.com>
2021-10-02 15:13:07 -0400

    basic login gang rise up

--- app/src/main/java/sheridan/teodored/myapplication/FirstFragment.kt
+++ app/src/main/java/sheridan/teodored/myapplication/FirstFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import sheridan.teodored.myapplication.databinding.FragmentFirstBinding
+import java.util.jar.Attributes
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,7 +17,7 @@ import sheridan.teodored.myapplication.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
+    private lateinit var auth: FirebaseAuth
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -31,9 +34,25 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+        if (FirebaseAuth.getInstance().currentUser == null){
+            println("Isnt logged")
+        }else{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val user = binding.UserName.text.toString()
+            val pass = binding.UserPass.text.toString()
+            println(user)
+            auth.signInWithEmailAndPassword(user,pass).addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+                }
+            }.addOnFailureListener { exception ->
+                println("nice cock")
+            }
         }
     }
 
@@ -41,4 +60,18 @@ class FirstFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+    fun Login(view: View){
+        val user = binding.UserName.text.toString()
+        val pass = _binding?.UserPass?.text.toString()
+        println(user)
+        auth.signInWithEmailAndPassword(user,pass).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
+        }.addOnFailureListener { exception ->
+            println("nice cock")
+        }
+    }
+
+
 }
--- app/src/main/java/sheridan/teodored/myapplication/MainActivity.kt
+++ app/src/main/java/sheridan/teodored/myapplication/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 import sheridan.teodored.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
